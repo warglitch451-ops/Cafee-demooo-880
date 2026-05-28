3cdbccaf657c3d5f9e591a7e6e1e0a91151bd262
warglitch451-ops <warglitch451@gmail.com>
2026-05-28 18:36:11 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            alert('Thank you! We will contact you shortly.');
+            form.reset();
+        });
+    }
+});
